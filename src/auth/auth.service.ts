79d0bdc6a44ef47db2d38ced2b52cee6fcefe909
BryantMagik <bryanedwardse@gmail.com>
2024-07-11 13:03:00 +0200

    rutas corregidas

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
-import { UsersService } from 'src/users/users.service';
+import { UsersService } from '../users/users.service';
 import { RegisterDto } from './dto/register.dto';
 import { Role_User } from '@prisma/client';
 import * as bcryptjs from 'bcryptjs';
