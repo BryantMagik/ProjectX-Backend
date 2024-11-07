58baebdaf084406fe452383f0eef65221b37eb85
BryantMagik <bryanedwardse@gmail.com>
2024-11-07 12:32:27 +0100

    PATCH ACTUALIZADO CORS

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   );
   app.enableCors({
     origin: 'http://localhost:4200',
-    methods: 'GET, POST, PUT, DELETE',
+    methods: 'GET, POST, PUT, DELETE, PATCH',
     allowedHeaders: 'Content-Type, Authorization',
   })
   setupSwagger(app);
