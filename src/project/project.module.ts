2387ec7660d390e90717ac5c94ac03048bf30d6d
BryantMagik <bryanedwardse@gmail.com>
2024-12-03 11:15:03 +0100

    workspace fixed

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -4,9 +4,10 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthModule } from 'src/auth/auth.module';
 import { ProjectService } from './project.service';
 import { UsersModule } from 'src/users/users.module';
+import { WorkspaceModule } from 'src/workspace/workspace.module';
 
 @Module({
-    imports: [PrismaModule, forwardRef(() => AuthModule),forwardRef(() => UsersModule)],
+    imports: [PrismaModule, forwardRef(() => AuthModule),forwardRef(() => UsersModule),WorkspaceModule],
     providers: [ProjectService],
     controllers: [ProjectController],
     exports: [ProjectService]
